6d5f5c1b2769fd97b9d2e5b68ec7fffd7cc8b478
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-12-11 18:37:17 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/Gifted-s/coupon-service
+
+go 1.17
+
+require (
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/jasonlvhit/gocron v0.0.1
+	go.uber.org/zap v1.23.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http" 
+	"sync"
+)
+
+var urls []string = []string{"https://google.com", "https://mongodb.com", "https://github.com", "https://heroku.com", "https://linkedin.com"}
+
+func main() {
+
+	sem := make(chan int, 4)
+	errChan := make(chan string, 1)
+	var wg *sync.WaitGroup
+	wg.Add(len(urls))
+	for _, url := range urls {
+	  go fetch(sem, url, wg, errChan)
+	} 
+	wg.Wait() 
+	close(errChan)
+
+}
+
+func fetch(sem chan int, url string, wg *sync.WaitGroup, errChan chan string) {
+	sem <- 1 
+	defer wg.Done()
+	resp, err := http.Get(url)
+	if err != nil {
+		errChan <- err.Error()
+	}
+	fmt.Println("===========================================================================================================")
+	fmt.Println(resp)
+	fmt.Println("===========================================================================================================")
+	<-sem
+}
